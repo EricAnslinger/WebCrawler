e3be25351303606faaa8f6d9eaf820579ac92eb6
Eric Anslinger <anslinger.eric.official@gmail.com>
2024-04-07 16:28:50 +0200

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -9,33 +9,27 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 
 public class Main {
-    public static void main(String[] args) {
-
+    public static void main(String[] args) {                                                        //Main
         URL getUrl = getURLInput(); //Website Input with Window (Call method getURLInput())
-
         if (getUrl != null) {  //getUrl should contain something to start crawling
             crawl(getUrl);  //Start crawling
         } else {
             System.exit(0);
         }
     }
-
     public static URL getURLInput() {
         URL userURL = null;
-
         while (userURL == null) {
             try {
                 // UserInput with JOptionPane Window
-                String getUserInput = JOptionPane.showInputDialog(null, "Please enter a URL!");
-
+                String getUserInput = JOptionPane.showInputDialog(null, "Please enter a valid URL!");
                 if(getUserInput == null){ //Exit programm, if getUserInput is null
                     System.exit(0);
                 }
-
                 userURL = new URL(getUserInput); // set userURL to common URL
             } catch (MalformedURLException e) {
                 // No valid URL? Then programm asks again
-                JOptionPane.showMessageDialog(null, "Please enter a valid URL", "Error", JOptionPane.ERROR_MESSAGE);
+                JOptionPane.showMessageDialog(null, "Please enter a valid URL", "Failed to resolve link", JOptionPane.ERROR_MESSAGE);
             }
         }
         return userURL; //return userURL to getURL
@@ -44,7 +38,6 @@ public class Main {
         try {
             String urlStr = url.toString(); //URL Object to String
             Document doc = Jsoup.connect(urlStr).get(); //Connect Website with Jsoup
-
             if(doc != null){
                 ArrayList<String> linksList = new ArrayList<>();
                 for(Element link : doc.select("a[href]")){ //Find all Link Elements , safe found link in link
